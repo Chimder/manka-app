ec8cf7ffdf5a8477bb8c4997c19ffac6db260c60
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-02 18:45:30 +0200

    2

--- src/components/a-recomend.tsx
+++ src/components/a-recomend.tsx
@@ -1,12 +1,12 @@
-import React, { useEffect } from 'react'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { RecomendAnim } from '@/shared/data/PopRecod'
-// import { trpc } from '@/shared/utils/trpc'
+// import React, { useEffect } from 'react'
+// import Link from 'next/link'
+// import { useRouter } from 'next/router'
+// import { RecomendAnim } from '@/shared/data/PopRecod'
+// // import { trpc } from '@/shared/utils/trpc'
 
-const AnimeRecomend = ({ name }: any) => {
-  return
-  const param = useRouter()
+// const AnimeRecomend = ({ name }: any) => {
+//   return
+//   const param = useRouter()
 
   // const {
   //   data: manga,
@@ -23,65 +23,65 @@ const AnimeRecomend = ({ name }: any) => {
   //   },
   // )
 
-  useEffect(() => {
-    refetch()
-  }, [param.query])
+//   useEffect(() => {
+//     refetch()
+//   }, [param.query])
 
-  if (isFetching) {
-    return <div></div>
-  }
-  return (
-    <div className=" z-100 -my-2  mt-0.5 flex-col md:my-2 md:flex md:items-center md:justify-center ">
-      <span className="text-xl font-semibold lg:text-sm">
-        {manga ? 'This Anime has Manga' : 'Recommended Anime'}
-      </span>
-      <div className="md:flex">
-        {manga ? (
-          <Link
-            key={manga.name}
-            href={`/manka/${manga.name}`}
-            className="float-left ml-[0.5px] mt-2 w-full rounded-2xl bg-transparent no-underline  md:flex md:items-center md:justify-center"
-          >
-            <div className="relative mx-2 flex ">
-              <div className="z-0 m-0 w-16 shrink-0 overflow-hidden rounded-sm ">
-                <div className="relative box-border">
-                  <img className="left-0  top-0 h-full w-full" src={manga?.img} alt="#" />
-                </div>
-              </div>
-              <div className="flex items-center  px-3 text-sm lg:text-[12px] md:hidden">
-                <span>{manga.name}</span>
-              </div>
-            </div>
-          </Link>
-        ) : (
-          RecomendAnim.map(manga => (
-            <>
-              <Link
-                key={manga.name}
-                href={`/manka/${manga.name}`}
-                className="float-left ml-[0.5px] mt-2 w-full  rounded-2xl bg-transparent no-underline "
-              >
-                <div className="relative mx-2 flex ">
-                  <div className="z-0 m-0 w-16 shrink-0 overflow-hidden rounded-sm ">
-                    <div className="relative box-border">
-                      <img
-                        className="left-0  top-0 h-full w-full"
-                        src={manga.img}
-                        alt={manga.name}
-                      />
-                    </div>
-                  </div>
-                  <div className="flex items-center  px-3 text-sm lg:text-[12px] md:hidden">
-                    <span>{manga.name}</span>
-                  </div>
-                </div>
-              </Link>
-            </>
-          ))
-        )}
-      </div>
-    </div>
-  )
-}
+//   if (isFetching) {
+//     return <div></div>
+//   }
+//   return (
+//     <div className=" z-100 -my-2  mt-0.5 flex-col md:my-2 md:flex md:items-center md:justify-center ">
+//       <span className="text-xl font-semibold lg:text-sm">
+//         {manga ? 'This Anime has Manga' : 'Recommended Anime'}
+//       </span>
+//       <div className="md:flex">
+//         {manga ? (
+//           <Link
+//             key={manga.name}
+//             href={`/manka/${manga.name}`}
+//             className="float-left ml-[0.5px] mt-2 w-full rounded-2xl bg-transparent no-underline  md:flex md:items-center md:justify-center"
+//           >
+//             <div className="relative mx-2 flex ">
+//               <div className="z-0 m-0 w-16 shrink-0 overflow-hidden rounded-sm ">
+//                 <div className="relative box-border">
+//                   <img className="left-0  top-0 h-full w-full" src={manga?.img} alt="#" />
+//                 </div>
+//               </div>
+//               <div className="flex items-center  px-3 text-sm lg:text-[12px] md:hidden">
+//                 <span>{manga.name}</span>
+//               </div>
+//             </div>
+//           </Link>
+//         ) : (
+//           RecomendAnim.map(manga => (
+//             <>
+//               <Link
+//                 key={manga.name}
+//                 href={`/manka/${manga.name}`}
+//                 className="float-left ml-[0.5px] mt-2 w-full  rounded-2xl bg-transparent no-underline "
+//               >
+//                 <div className="relative mx-2 flex ">
+//                   <div className="z-0 m-0 w-16 shrink-0 overflow-hidden rounded-sm ">
+//                     <div className="relative box-border">
+//                       <img
+//                         className="left-0  top-0 h-full w-full"
+//                         src={manga.img}
+//                         alt={manga.name}
+//                       />
+//                     </div>
+//                   </div>
+//                   <div className="flex items-center  px-3 text-sm lg:text-[12px] md:hidden">
+//                     <span>{manga.name}</span>
+//                   </div>
+//                 </div>
+//               </Link>
+//             </>
+//           ))
+//         )}
+//       </div>
+//     </div>
+//   )
+// }
 
-export default AnimeRecomend
+// export default AnimeRecomend
