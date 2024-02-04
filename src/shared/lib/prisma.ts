5b75638828453c1b65a10c8948d092ca6e8581b3
Chimder <127688661+Chimder@users.noreply.github.com>
2024-02-04 21:18:11 +0200

    prisma

--- src/shared/lib/prisma.ts
+++ src/shared/lib/prisma.ts
@@ -1,10 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 
-declare global {
-  var prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+  return new PrismaClient()
 }
 
-const prisma = global.prisma || new PrismaClient()
-if (process.env.NODE_ENV === 'development') global.prisma = prisma
+declare global {
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
+
+if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
